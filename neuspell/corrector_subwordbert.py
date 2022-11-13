aaba52f0076020b7265fe41c361f494c3d56d48f
UthpalaPitawela <uthpala.isiru@gmail.com>
2022-11-13 21:00:30 +0530

    changes done

--- neuspell/corrector_subwordbert.py
+++ neuspell/corrector_subwordbert.py
@@ -255,7 +255,7 @@ class BertChecker(Corrector):
                     batch_labels, batch_sentences = batch_labels_, batch_sentences_
                 batch_bert_inp = {k: v.to(DEVICE) for k, v in batch_bert_inp.items()}
                 # set batch data for others
-=                batch_labels, batch_lengths = labelize(batch_labels, vocab)
+=               batch_labels, batch_lengths = labelize(batch_labels, vocab)
 =                # batch_lengths = batch_lengths.to(device)
                 batch_labels = batch_labels.to(DEVICE)
 =                # forward
