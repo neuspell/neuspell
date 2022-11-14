6b5799174fef200b71b7db0f4e3e31bb623097c7
UthpalaPitawela <uthpala.isiru@gmail.com>
2022-11-14 07:31:53 +0530

    changes done

--- neuspell/seq_modeling/helpers.py
+++ neuspell/seq_modeling/helpers.py
@@ -741,7 +741,7 @@ def bert_tokenize_for_valid_examples(batch_orginal_sentences, batch_noisy_senten
         # "token_type_ids": []
     }
     if len(valid_idxs) > 0:
-        batch_encoded_dicts = [BERT_TOKENIZER.encode_plus(tokens, add_special_tokens=True, truncation = True, padding="max_length", is_split_into_words=True,return_attention_mask = True) for tokens in batch_tokens]
+        batch_encoded_dicts = [BERT_TOKENIZER.encode_plus(tokens,max_length=552, add_special_tokens=True, truncation = True, padding="max_length", is_split_into_words=True,return_attention_mask = True) for tokens in batch_tokens]
         batch_attention_masks = pad_sequence(
             [torch.tensor(encoded_dict["attention_mask"]) for encoded_dict in batch_encoded_dicts], batch_first=True,
             padding_value=0)
